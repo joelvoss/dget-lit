7950221b6056d2215fde54234f1fd8c202b87b2f
Joel Voss <mail@joelvoss.com>
2020-10-31 21:15:16 +0100

    refactor: migrate to `@jvdx/core`

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,17 @@
-/**
- * dlv safely gets a value from a nested object via a dot-notated path.
- * In addition you have the ability to provide a fallback value to be returned
- * if the full key path does not exists or the value is undefined.
- */
-export const dget = <ObjectType extends { [key: string]: any }, DefaultType>(
-  obj: ObjectType,
-  key: string | string[],
-  def?: DefaultType,
-  p?: number,
-) => {
-  p = 0;
-  key =
-    typeof (key as string).split === 'function'
-      ? (key as string).replace(/\[([\w\d]+)\]/g, '.$1').split('.')
-      : key;
-  while (obj && p < key.length) obj = obj[key[p++]];
-  return obj === undefined || p < key.length ? def : obj;
-};
+// dget safely gets a value from a nested object via a dot-notated path.
+// In addition you have the ability to provide a fallback value to be returned
+// if the full key path does not exists or the value is undefined.
+export function dget<ObjectType extends { [key: string]: any }, DefaultType>(
+	obj: ObjectType,
+	key: string | string[],
+	def?: DefaultType,
+	p?: number,
+) {
+	p = 0;
+	key =
+		typeof (key as string).split === 'function'
+			? (key as string).replace(/\[([\w\d]+)\]/g, '.$1').split('.')
+			: key;
+	while (obj && p < key.length) obj = obj[key[p++]];
+	return obj === undefined || p < key.length ? def : obj;
+}
